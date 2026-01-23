faf5a8da6e69d688931fb1567228156482552829
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-01-23 16:43:44 +0530

    Learn Opeations in JavaScript

--- 01_basics/03-conversionOperation.js
+++ 01_basics/03-conversionOperation.js
@@ -1,9 +1,9 @@
 let score = true
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" -> 33
 // "33abc" -> NaN
@@ -12,7 +12,7 @@ console.log(valueInNumber);
 let isLoggedIn ="harsh"
 
 let bolleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(bolleanIsLoggedIn);
+// console.log(bolleanIsLoggedIn);
 
 // 1 -> true; 0-> false
 // "" -> false
@@ -21,5 +21,42 @@ console.log(bolleanIsLoggedIn);
 let someNumber = 19
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **--------Operations---------**
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " harsh"   // " harsh" gives space  
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "3");
+// console.log(1 + "2" + 3);
+// console.log("1" + 2 + 3);
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(-true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
