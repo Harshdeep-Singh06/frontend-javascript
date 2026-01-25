eaaebbc3bc9e96ef4ee3a7ec2dad216786a1e134
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-01-25 14:39:31 +0530

    Learn numbers

---
+++ 01_basics/06-nums_and_math.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1)); // control digits after decimal
+
+const otherNumber = 123.8966
+
+console.log(otherNumber.toPrecision(4)); //control total digits 
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
