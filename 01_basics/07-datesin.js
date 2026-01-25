623e8c28c2516afbd4688027c3c9c6ff4badcafe
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-01-25 16:02:00 +0530

    Learn dates in Js

---
+++ 01_basics/07-datesin.js
@@ -0,0 +1,28 @@
+// Dates
+
+let myDate = new Date()
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2006, 1, 10)
+// let myCreatedDate = new Date(2006, 1, 10, 5, 3)
+let myCreatedDate = new Date("10-02-2006")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time`
+
+
