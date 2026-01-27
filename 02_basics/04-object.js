c8464d98aa81bb55840b5cdfaccfb52c28af0153
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-01-27 20:40:55 +0530

    Learn Objects part-3 & Overview API

--- 02_basics/04-object.js
+++ 02_basics/04-object.js
@@ -39,10 +39,36 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename: "JavaScript",
+    price: "999",
+    courseInstructor: "OOlong"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name" = "harsh",
+//     "coursename": "js",
+//     "price": "free",
+// }
+
+[
+    {},
+    {},
+    {}
+]
