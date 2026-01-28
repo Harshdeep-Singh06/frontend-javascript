72bc139593768c84a9f047132bbdab88e7b7cace
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-01-28 12:43:27 +0530

    Learn Scopes in JS

--- 03_basics/02-scopes.js
+++ 03_basics/02-scopes.js
@@ -3,11 +3,49 @@ let a =300
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner:" ,a);
+    // console.log("Inner:" ,a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "harsh"
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+if (true){
+    const username = "harsh"
+    if (username === "harsh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//-----------------------//--------------------//
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
