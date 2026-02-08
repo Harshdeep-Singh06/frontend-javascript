198005d33e68356d877221d52893e1f318b8fafa
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-02-08 13:37:11 +0530

    Practice objects questions

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -7,14 +7,20 @@
 // console.log(arr.splice(1,3))
 // console.log(arr);
 
-let nums = [1,2,3];
-let square = nums.map(n => n*n);
-// console.log(nums);
-let user = {
-  name: "Harsh",
-  greet: function() {
-    console.log("Hello");
-  }
-};
+// let nums = [1,2,3];
+// let square = nums.map(n => n*n);
+// // console.log(nums);
+// let user = {
+//   name: "Harsh",
+//   greet: function() {
+//     console.log("Hello");
+//   }
+// };
 
-user.greet();
+// user.greet();
+
+let person = {
+    name: "Harsh",
+    age: 19
+}
+console.log(Object.entries(person));
