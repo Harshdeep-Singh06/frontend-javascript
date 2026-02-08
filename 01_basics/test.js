48bdc86e78ee5e1f2549cf2953a3763b659625e4
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-02-08 13:18:30 +0530

    Revise functions

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,9 +1,20 @@
 // console.log("Harsh")
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 // arr.push(6);
 
 // console.log(arr.indexOf(4));
 // console.log(arr.slice(0,4));
-console.log(arr.splice(1,3))
-console.log(arr);
+// console.log(arr.splice(1,3))
+// console.log(arr);
 
+let nums = [1,2,3];
+let square = nums.map(n => n*n);
+// console.log(nums);
+let user = {
+  name: "Harsh",
+  greet: function() {
+    console.log("Hello");
+  }
+};
+
+user.greet();
